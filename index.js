cc9ce696a131e6a77bfe3e46df7ffc5747e60180
vectorg3 <diglytail@gmail.com>
2023-11-06 12:44:14 +0300

    https settings added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import fs from 'fs';
 import { signUpValidation, loginValidation } from './validation.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
 import cors from 'cors';
+import helmet from 'helmet';
+import https from 'https';
 
 mongoose.set('strictQuery', false);
 mongoose
     // .connect(process.env.MONGODB_URI)
-    .connect('mongodb+srv://admin:392311@taskmanager.rcddhdh.mongodb.net/Task-Manager?retryWrites=true&w=majority')
+    .connect(
+        'mongodb+srv://admin:392311@taskmanager.rcddhdh.mongodb.net/Task-Manager?retryWrites=true&w=majority'
+    )
     .then(() => {
         console.log('Database Connected');
     })
@@ -18,21 +23,26 @@ mongoose
 
 const app = express();
 
+const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem'),
+};
+
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/signUp', signUpValidation, UserController.signUp);
 app.get('/auth/me', checkAuth, UserController.getMe);
-app.put('/tasks', checkAuth, UserController.saveTasks)
+app.put('/tasks', checkAuth, UserController.saveTasks);
 app.get('/', (req, res) => {
-    res.json({message: 'hello'})
-})  
+    res.json({ message: 'hello' });
+});
 
-
-app.listen(process.env.PORT || 4444, (err) => {
+https.createServer(options, app).listen(4444, (err) => {
     if (err) {
-        return console.log(err);
+        console.log(err);
     }
     console.log('Server started');
 });
